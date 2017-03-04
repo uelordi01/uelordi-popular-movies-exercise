55fc64ad640b7bfb2dfa4444feeb93c0f1db5b2c
uelordi <elordiae@gmail.com>
2017-03-04 16:30:20 +0100

    project created

---
+++ MyApplication/app/src/main/java/udacity/uelordi/com/popularmovies/FetchVideoList.java
@@ -0,0 +1,25 @@
+package udacity.uelordi.com.popularmovies;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by uelordi on 04/03/17.
+ */
+
+public class FetchVideoList extends AsyncTask<String,Void,String>
+{
+    @Override
+    protected String doInBackground(String... sortedBy) {
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+}
---
+++ MyApplication/app/src/main/java/udacity/uelordi/com/popularmovies/VideoListActivity.java
@@ -0,0 +1,13 @@
+package udacity.uelordi.com.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_list);
+    }
+}
