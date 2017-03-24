aa78d7e1c2eec31cdbf0bf40391a999a281c884a
Unai Elordi Hidalgo <uelordi@vicomtech.org>
2017-03-24 14:39:09 +0100

    some Db contract modifications to identify the Uri

--- popular-movies-exercise/app/src/main/java/udacity/uelordi/com/popularmovies/database/MovieContract.java
+++ popular-movies-exercise/app/src/main/java/udacity/uelordi/com/popularmovies/database/MovieContract.java
@@ -26,7 +26,7 @@ public class MovieContract {
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIES).build();
         public static final String COLUMN_MOVIE_ID = "movie_id";
-        public static final String TABLE_NAME = "movie";
+        public static final String TABLE_NAME = "movies";
         public static final String COLUMN_ID = "id";
         public static final String COLUMN_TITLE = "title";
         public static final String COLUMN_USER_RATING = "user_Rating";
