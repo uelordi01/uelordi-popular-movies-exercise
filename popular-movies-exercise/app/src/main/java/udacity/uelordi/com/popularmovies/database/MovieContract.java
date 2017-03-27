8c0faeb4aea90b23b9f9e949d0b690cdbbd20d42
uelordi <elordiae@gmail.com>
2017-03-28 00:18:33 +0200

    Adding insert and query in the content provider

--- popular-movies-exercise/app/src/main/java/udacity/uelordi/com/popularmovies/database/MovieContract.java
+++ popular-movies-exercise/app/src/main/java/udacity/uelordi/com/popularmovies/database/MovieContract.java
@@ -1,5 +1,6 @@
 package udacity.uelordi.com.popularmovies.database;
 
+import android.content.ContentUris;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -17,8 +18,11 @@ public class MovieContract {
 
     // Define the possible paths for accessing data in this contract
     // This is the path for the "tasks" directory
-    public static final String PATH_MOVIES = "movies";
-
+    public static final String PATH_POPULAR = "most_popular";
+    public static final String PATH_RATED = "highest_rated";
+    public static final String PATH_FAVORITES="favorites";
+    public static final String PATH_MOVIES="table_movies";
+    public static final String COLUMN_MOVIE_ID_FKEY = "movie_id";
 
     public static final class MovieEntry implements BaseColumns {
 
@@ -26,13 +30,81 @@ public class MovieContract {
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIES).build();
         public static final String COLUMN_MOVIE_ID = "movie_id";
+
         public static final String TABLE_NAME = "movies";
         public static final String COLUMN_ID = "id";
         public static final String COLUMN_TITLE = "title";
         public static final String COLUMN_USER_RATING = "user_Rating";
         public static final String COLUMN_SYNOPSYS = "synopsys";
         public static final String COLUMN_IMAGE_URL = "poster_path";
+
+        public final static String CREATE_TABLE = "CREATE TABLE " + MovieContract.MovieEntry.TABLE_NAME + " (" +
+                _ID + " INTEGER PRIMARY KEY, " +
+                COLUMN_ID + " TEXT NOT NULL, " +
+                COLUMN_TITLE + " TEXT NOT NULL, " +
+                MovieContract.MovieEntry.COLUMN_SYNOPSYS + " TEXT NOT NULL, " +
+                MovieContract.MovieEntry.COLUMN_USER_RATING +  "REAL NOT NULL," +
+                MovieContract.MovieEntry.COLUMN_IMAGE_URL   + " TEXT NOT NULL)";
+
+        public static Uri buildMovieUri(long id) {
+            return ContentUris.withAppendedId(BASE_CONTENT_URI, id);
+        }
+
+        public static long getIdFromUri(Uri uri) {
+            return ContentUris.parseId(uri);
+        }
         //public static final String COLUMN_RELEASE_DATE = "title";
 
+        }
+    public static final class PopularEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =  BASE_CONTENT_URI.buildUpon().appendPath(PATH_POPULAR).build();
+
+
+        public static final String TABLE_NAME = "most_popular_movies";
+
+        public static final String CREATE_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        COLUMN_MOVIE_ID_FKEY + " INTEGER NOT NULL, " +
+
+                        " FOREIGN KEY (" + COLUMN_MOVIE_ID_FKEY + ") REFERENCES " +
+                        MovieEntry.TABLE_NAME + " (" + MovieEntry._ID + ") " +
+
+                        " );";
+        }
+    public static final class HighestRatedEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =  BASE_CONTENT_URI.buildUpon().appendPath(PATH_RATED).build();
+
+
+        public static final String TABLE_NAME = "highest_rated_movies";
+
+        public static final String CREATE_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        COLUMN_MOVIE_ID_FKEY + " INTEGER NOT NULL, " +
+
+                        " FOREIGN KEY (" + COLUMN_MOVIE_ID_FKEY + ") REFERENCES " +
+                        MovieEntry.TABLE_NAME + " (" + MovieEntry._ID + ") " +
+
+                        " );";
     }
+    public static final class FavoritesEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =  BASE_CONTENT_URI.buildUpon().appendPath(PATH_FAVORITES).build();
+
+
+        public static final String TABLE_NAME = "favorite_movies";
+
+        public static final String CREATE_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        COLUMN_MOVIE_ID_FKEY + " INTEGER NOT NULL, " +
+
+                        " FOREIGN KEY (" + COLUMN_MOVIE_ID_FKEY + ") REFERENCES " +
+                        MovieEntry.TABLE_NAME + " (" + MovieEntry._ID + ") " +
+
+                        " );";
+    }
+
+
+
 }
